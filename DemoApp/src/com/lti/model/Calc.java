3279c7383446730984a19d0f35e89019c87366fe
MohanNaik01 <76149011+MohanNaik01@users.noreply.github.com>
2020-12-18 16:36:35 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,5 +8,7 @@ public class Calc {
 	public void diff(int num1,int num2) {
 		System.out.println("Diff= "+(num1-num2));
 	}
-	
+	public void Multi(int num1,int num2) {
+		System.out.println("Multi= "+(num1*num2));
+	}
 }
